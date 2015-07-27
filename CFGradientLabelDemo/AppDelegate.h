eeddc5f868ad3a34b1f848b613c2bdb66e0691bd
yuchuanfeng <286428125@qq.com>
2015-07-27 18:29:58 +0800

    新建

---
+++ CFGradientLabelDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CFGradientLabelDemo
+//
+//  Created by 于 传峰 on 15/7/27.
+//  Copyright (c) 2015年 于 传峰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CFGradientLabelDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CFGradientLabelDemo
+//
+//  Created by 于 传峰 on 15/7/27.
+//  Copyright (c) 2015年 于 传峰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
